f4e911c39ab5a196e7b8d7d8e6f461277e8edf4f
OtsuruAyami <>
2025-08-21 15:56:11 +0900

    処理の学習

--- example-app/app/Http/Controllers/TestController.php
+++ example-app/app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Userモデルファイルの場所を示したuse宣言
+use App\Models\User;
 
 class TestController extends Controller
 {
     //どのファイルを表示するかを指定
     public function test() {
-        return view('test');
+        // usersテーブルに入っているレコードを全て取得し、$users変数に代入
+        $users = User::all();
+        //ビューファイルを表示する時に$users変数も受け渡すためのコード（compact関数で受け渡し）
+        return view('test', compact('users'));
     }
 }
