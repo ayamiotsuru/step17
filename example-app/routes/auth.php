18d6b3df6eba46b9016982c32affff7bae082001
OtsuruAyami <>
2025-08-21 13:56:56 +0900

    テスト用コントローラー

--- example-app/routes/auth.php
+++ example-app/routes/auth.php
@@ -12,9 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    // 画面表示用
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
+    // 投稿データ保存用
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
